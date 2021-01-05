e09711339d0495c4064bf1bb824a4cb32d5e6c37
AETSRound <qkrcksdna@naver.com>
2021-01-05 18:00:13 +0900

    코테추가2

--- static/talk.js
+++ static/talk.js
@@ -9,19 +9,31 @@ function solution(list){
     //닉네임 변경
     const CHANGE = "Change";
 
-    // let msgMap = {
-    //     Enter:"들어왔습니다",
-    //     Leave:"나갔습니다",
-    // }
-
+    let msgMap = {
+        Enter:"들어왔습니다",
+        Leave:"나갔습니다",
+    }
+    let msgList = [];
     // Action UID Nam 으로 구성된 string이 배열의 element
     let systemMap = {};
     for(let _map of tList){
         let _tMap = [];
         _tMap = _map.split(" ");    
         console.log(_tMap);
-    }
+        if(_tMap[0]===ENTER){
+            systemMap[_tMap[1]] = _tMap[2];
+            msgList.push(`${_tMap[2]}님이 ${msgMap[_tMap[0]]}`);
+        }
+        else if(_tMap[0]===LEAVE){
+            msgList.push(`${_tMap[2]}님이 ${msgMap[_tMap[0]]}`);
+        }
+        else{
+            systemMap[_tMap[1]] = _tMap[2];
+        }
 
+    }
+    console.log(msgList);
+    //메세지 넣는거랑 map의 id의 일관성 유지하는걸 따로할 필요가 있다.
 
     return tList;
 }
