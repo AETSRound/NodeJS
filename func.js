5a12bfc200bb819aa626f65be3c46a539a243cf1
unknown <qkrcksdna@naver.com>
2021-01-28 10:29:37 +0900

    JS의 모듈과 Node의 모듈

--- func.js
+++ func.js
@@ -2,7 +2,7 @@ const value = require('./var');
 //이 방식은 Node의 모듈 시스템.
 
 //JS의 모듈 시스템도 있다.
-import {odd, even} from "./var.js"
+// import {odd, even} from "./var.js"
 //이렇게 지원은 하긴 하지만, node에서의 module 시스템을 알아둬야 읽고 사용이 가능하다.
 
 //require - module.exports 와 import - default export 와는 동작 방식이 다르다.
